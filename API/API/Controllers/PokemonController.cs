8ad755c95f3c9234b1c9e92a1f2843dee3f1bc93
Sasha-Gill <54946194+Sasha-Gill@users.noreply.github.com>
2020-02-19 14:54:39 +0100

    lmkn

---
+++ API/API/Controllers/PokemonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/pokemon")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+
+        public static List<pokemon> pokemons = new List<pokemon>()
+        {
+            new pokemon() {Name = "Bergfors", Strenght = 69 },
+            new pokemon() {Name = "flog", Strenght = 300 },
+            new pokemon() {Name = "Tirak", Strenght = 200 }
+        };
+
+        [HttpGet]
+        public ActionResult get()
+        {
+
+            /*pokemon p = new pokemon();
+            p.Name = "Bergfors";
+            p.Strenght = 69;*/
+
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ API/API/Controllers/pokemon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+	public class pokemon
+	{
+		public string Name
+		{
+			get
+			{
+				return Name;
+			}
+			set
+			{
+				Name = value.ToUpper();
+			}
+		}
+
+		public int Strenght { get; set; }
+
+	}
+}
